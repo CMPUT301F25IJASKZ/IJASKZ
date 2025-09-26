f8ddb5e4db8b9f118630b0e4c8b187fc71c35d8f
Ishayat Arik Chowdhury <ishayatarik17@gmail.com>
2025-09-26 15:50:17 -0600

    Made changes

--- app/src/main/java/com/example/ijaskz/Shape.java
+++ app/src/main/java/com/example/ijaskz/Shape.java
@@ -3,5 +3,10 @@ package com.example.ijaskz;
 abstract public class Shape {
     protected int x;
     protected int y;
+<<<<<<< HEAD
     String color = "Mahogany Red";
 }
+=======
+    String color = "Purple";
+}
+>>>>>>> 12b1348 (Changed the color to purple)
